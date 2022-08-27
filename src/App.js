566b7b5dbe61b5e6d95e6e84c3cefa13a720e557
Corneliu Laur <cl179971@gmail.com>
2022-08-27 23:27:29 +0100

    added folder structure in src folder & dummy data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import './App.css';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    return <div>App</div>;
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react';
-
+import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 
 // Render the whole React App into the element with id=root (hooks the entire react application to the 'root' div)
